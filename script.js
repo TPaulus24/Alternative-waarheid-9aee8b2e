d96e2676b9945a1e54ac5ed62c170a8be3241b36
FIRST_TOM LAST_PAULUS <54597104+TPaulus24@users.noreply.github.com>
2020-02-06 17:12:12 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,49 +1,62 @@
-let antwoorden = ['Parijs', '8', 'Ijsselmeer']
-let automerken = ["Volkswagen", "Audi", "Opel", "Porsche", "BMW", "Mercedes", "Mercedes-Benz"]
-let wadde = ["Texel", "Vlieland", "Terschelling", "Ameland", "Schiermonnikoog"]
+const vragen = [
+    "Wat is de hoofdstad van Frankrijk?",
+    "Hoeveel poten heeft een spin?",
+    "Wat is het grootste meer van Nederland?",
+    "Noem een duits automerk",
+    "Noem een waddeneiland",
+];
+const antwoorden = [
+    "Parijs", "8", "Ijsselmeer", ["Volkswagen", "Audi", "Opel", "Porsche", "BMW", "Mercedes", "Mercedes-Benz"],
+    ["Texel", "Vlieland", "Terschelling", "Ameland", "Schiermonnikoog"],
+];
+
+document.getElementById("vraag1").innerHTML = vragen[0];
+document.getElementById("vraag2").innerHTML = vragen[1];
+document.getElementById("vraag3").innerHTML = vragen[2];
+document.getElementById("vraag4").innerHTML = vragen[3];
+document.getElementById("vraag5").innerHTML = vragen[4];
+
 
 function controleer() {
     let goed = 0;
-    let input1 = document.getElementById("vraag1").value;
-    if (input1 == antwoorden[0]) {
-        document.getElementById("vraag1").style.backgroundColor = "#42ff6b";
+    let antwoord1 = document.getElementById("antwoord1").value;
+    if (antwoord1 === antwoorden[0]) {
+        document.getElementById("antwoord1").style.backgroundColor = "#42ff6b";
         goed += 1;
     } else {
-        document.getElementById("vraag1").style.backgroundColor = "#ff4f42";
+        document.getElementById("antwoord1").style.backgroundColor = "#ff4f42";
     }
 
-    let input2 = document.getElementById("vraag2").value;
-    if (input2 == antwoorden[1]) {
-        document.getElementById("vraag2").style.backgroundColor = "#42ff6b";
+    let antwoord2 = document.getElementById("antwoord2").value;
+    if (antwoord2 === antwoorden[1]) {
+        document.getElementById("antwoord2").style.backgroundColor = "#42ff6b";
         goed += 1;
     } else {
-        document.getElementById("vraag2").style.backgroundColor = "#ff4f42";
+        document.getElementById("antwoord2").style.backgroundColor = "#ff4f42";
     }
 
-    let input3 = document.getElementById("vraag3").value;
-    if (input3 == antwoorden[2]) {
-        document.getElementById("vraag3").style.backgroundColor = "#42ff6b";
+    let antwoord3 = document.getElementById("antwoord3").value;
+    if (antwoord3 === antwoorden[2]) {
+        document.getElementById("antwoord3").style.backgroundColor = "#42ff6b";
         goed += 1;
     } else {
-        document.getElementById("vraag3").style.backgroundColor = "#ff4f42";
+        document.getElementById("antwoord3").style.backgroundColor = "#ff4f42";
     }
-
-    let input4 = document.getElementById("vraag4").value;
-    if (automerken.includes(input4)) {
-        document.getElementById("vraag4").style.backgroundColor = "#42ff6b";
+    let antwoord4 = document.getElementById("antwoord4").value;
+    if (antwoorden[3].includes(antwoord4)) {
+        document.getElementById("antwoord4").style.backgroundColor = "#42ff6b";
         goed += 1;
     } else {
-        document.getElementById("vraag4").style.backgroundColor = "#ff4f42";
+        document.getElementById("antwoord4").style.backgroundColor = "#ff4f42";
     }
-
-    let input5 = document.getElementById("vraag5").value;
-    if (wadde.includes(input5)) {
-        document.getElementById("vraag5").style.backgroundColor = "#42ff6b";
+    let antwoord5 = document.getElementById("antwoord5").value;
+    if (antwoorden[4].includes(antwoord5)) {
+        document.getElementById("antwoord5").style.backgroundColor = "#42ff6b";
         goed += 1;
     } else {
-        document.getElementById("vraag5").style.backgroundColor = "#ff4f42";
+        document.getElementById("antwoord5").style.backgroundColor = "#ff4f42";
     }
-    if (goed == 5) {
+    if (goed === 5) {
         document.getElementById("goedeAntwoorden").innerHTML = "Alle antwoorden zijn goed!";
     } else {
         document.getElementById("goedeAntwoorden").innerHTML = goed + " van de 5 antwoorden zijn goed!";
